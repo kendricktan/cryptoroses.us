580b4d95c4ad7f64678d32426e7092339005f334
Kendrick Tan <kendricktan0814@gmail.com>
2018-01-27 00:12:11 +1000

    styling

--- client/pages/eth.js
+++ client/pages/eth.js
@@ -2,18 +2,28 @@ import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Web3Container from '../lib/Web3Container'
+import { Image, Form, Segment, Button, TextArea, Input } from 'semantic-ui-react'
 
 class RoseViaETH extends React.Component {
-  state = { sendMemo: '', readMemo: '', readRoseType: -1, readHasRose: false, readHash: '' }
+  state = { buyRoseType: 'white', sendFrom: '', sendTo: '', sendMemo: '', readMemo: '', readRoseType: -1, readHasRose: false, readHash: '' }
+
+  handleSelectedRoseChange = (e, { value }) => this.setState({ buyRoseType: value })
 
   componentDidMount = () => {
     this.checkRose()
   }
 
-  buyRose = (a) => async () => {
+  buyRose = async () => {
     const { accounts, contract, web3 } = this.props
-    const { sendMemo } = this.state
-    await contract.buyRoseETH(sendMemo, { from: accounts[0], to: contract.address, value: web3.utils.toWei(a, 'ether') })
+    const { sendMemo, sendFrom, sendTo, buyRoseType } = this.state
+    const memo = JSON.stringify({
+      memo: sendMemo,
+      to: sendTo,
+      from: sendFrom
+    })
+    const ethPrice = this.getRoseETH(buyRoseType)
+
+    await contract.buyRoseETH(memo, { from: accounts[0], to: contract.address, value: web3.utils.toWei(ethPrice, 'ether') })
   }
 
   checkRose = async () => {
@@ -30,9 +40,17 @@ class RoseViaETH extends React.Component {
     })
   }
 
+  getRoseETH = (color) => {
+    if (color === 'gold') return '0.25'
+    else if (color === 'white') return '0.05'
+    else if (color === 'pink') return '0.02'
+    else if (color === 'red') return '0.01'
+  }
+
   render() {
     const { contract, web3 } = this.props
-    const { readHasRose, readRoseType, readMemo, readHash } = this.state
+    const { readHasRose, readRoseType, readMemo, readHash, buyRoseType } = this.state
+    const ethPrice = this.getRoseETH(buyRoseType)
 
     return (
       <div>
@@ -40,24 +58,81 @@ class RoseViaETH extends React.Component {
           readHasRose ?
             (
               <div>
-                <h1>Rose Status</h1>
-                You've already bought a rose<br />
-                Hash: {readHash}
+                <h2 style={{ fontFamily: "'Open Sans', sans-serif", color: '#1e272e' }}>It appears you already bought a rose :)</h2> <br/>                
+                <b>Hash</b>: <a href={'/check?hash=' + readHash}>{readHash}</a>
               </div>
             ) :
             (
               <div>
-                <h1>Buy roses</h1>
+                <h2 style={{ fontFamily: "'Open Sans', sans-serif", color: '#1e272e' }}>Attach a message alongside your Rose</h2>
 
-                Memo: <input type="text" onChange={(e) => this.setState({ sendMemo: e.target.value })} /> <br />
+                {
+                  buyRoseType === 'gold' ?
+                    <Image src='//www.theforeverrose.com/media/dipped-roses/gold-rose-4.jpg' size='medium' centered /> :
+                    buyRoseType === 'white' ?
+                      <Image src='//www.doctorsja.com/wp-content/uploads/2013/06/small__2343714916.jpg' size='medium' centered /> :
+                      buyRoseType === 'pink' ?
+                        <Image src='//storiesthatmatter.files.wordpress.com/2014/10/pink-rose-1280x1024.jpg' size='medium' centered /> :
+                        <Image src='//www.fullblossomflorist.com/wp-content/uploads/2016/02/0000350_single-red-rose-600x600.jpeg' size='medium' centered />
+                } <br />
 
-                <button onClick={this.buyRose('0.25')}>Buy Gold</button> &nbsp;
-                <button onClick={this.buyRose('0.05')}>Buy White</button> &nbsp;
-                <button onClick={this.buyRose('0.02')}>Buy Pink</button> &nbsp;
-                <button onClick={this.buyRose('0.01')}>Buy Red</button>
+                <Form>
+                  <Form.Group widths='equal' inline>
+                    <Form.Radio
+                      fluid
+                      label='Gold Rose'
+                      name='radioGroup'
+                      value='gold'
+                      checked={this.state.buyRoseType === 'gold'}
+                      onChange={this.handleSelectedRoseChange}
+                    />
+                    <Form.Radio
+                      fluid
+                      label='White Rose'
+                      name='radioGroup'
+                      value='white'
+                      checked={this.state.buyRoseType === 'white'}
+                      onChange={this.handleSelectedRoseChange}
+                    />
+                    <Form.Radio
+                      fluid
+                      label='Pink Rose'
+                      name='radioGroup'
+                      value='pink'
+                      checked={this.state.buyRoseType === 'pink'}
+                      onChange={this.handleSelectedRoseChange}
+                    />
+                    <Form.Radio
+                      fluid
+                      label='Red Rose'
+                      name='radioGroup'
+                      value='red'
+                      checked={this.state.buyRoseType === 'Red'}
+                      onChange={this.handleSelectedRoseChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Field>
+                    <Input onChange={(e) => this.setState({ sendFrom: e.target.value })} fluid icon='send' placeholder='Sender name' />
+                  </Form.Field>
+
+                  <Form.Field>
+                    <Input onChange={(e) => this.setState({ sendTo: e.target.value })} fluid icon='user' placeholder='Recipient name' />
+                  </Form.Field>
+
+                  <Form.Field>
+                    <TextArea placeholder='Memo: E.g. Lots of love' onChange={(e) => this.setState({ sendMemo: e.target.value })} />
+                  </Form.Field>
+                </Form>
 
                 <br />
-                <button onClick={this.checkRose}>Check Rose Status</button>
+                <Button
+                  onClick={this.buyRose}
+                  fluid
+                  basic
+                  color={buyRoseType === 'gold' ? 'yellow' : buyRoseType}>
+                  Buy {buyRoseType} rose ({ethPrice} ETH)
+                </Button>
                 <br />
               </div>
             )
@@ -69,9 +144,6 @@ class RoseViaETH extends React.Component {
 
 export default () => (
   <div>
-    <Head>
-      <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css' />
-    </Head>
     <Web3Container
       renderLoading={() => <div>Loading Dapp Page...</div>}
       render={({ accounts, contract, web3 }) => (
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,22 +1,106 @@
 import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
-import CryptoRosesTemplate from './template'
+import BuyETH from './eth.js'
 import { Menu, Grid, Segment, Button } from 'semantic-ui-react'
 
+class HomeSegment extends React.Component {
+  render() {
+    return (
+      <Segment basic>
+        <div>
+          <div style={{ textAlign: 'center' }}>
+            <img src='//i.imgur.com/6Hrivqn.png' /><br />
+            <h2 style={{ fontFamily: "'Open Sans', sans-serif", color: '#1e272e' }}>
+              Buy roses on the Ethereum blockchain via ETH or GRLC
+              </h2><br />
+            <Button basic color='violet' content='Buy with Ethereum (ETH)' onClick={() => this.props.handleButtonClick({ name: 'buyViaETH' })} />
+            <Button basic color='olive' content='Buy with Garlicoin (GRLC)' onClick={() => this.props.handleButtonClick({ name: 'buyViaGRLC' })} />
+          </div>
+        </div>
+      </Segment>
+    )
+  }
+}
 
-export default () =>
-  CryptoRosesTemplate(
-    <Segment basic>
+class CryptoRosesTemplate extends React.Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleButtonClick = ({ name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
       <div>
-        <div style={{ textAlign: 'center' }}>
-          <img src='//i.imgur.com/6Hrivqn.png' /><br />
-          <h2 style={{ fontFamily: "'Open Sans', sans-serif", color: '#1e272e' }}>
-            Buy roses on the Ethereum blockchain via ETH or GRLC
-          </h2><br/>
-          <Link href='/eth'><Button basic color='violet' content='Buy with Ethereum (ETH)' /></Link> &nbsp;&nbsp;
-          <Link href='/grlc'><Button basic color='olive' content='Buy with Garlicoin (GRLC)' /></Link>
-        </div>        
-      </div>
-    </Segment>
-  )
+        <Head>
+          <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css"
+          />
+          <link href="//fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
+        </Head>
+
+        <Grid columns='equal'>
+          <Grid.Column></Grid.Column>
+          <Grid.Column width={8}>
+            <Menu text pointing secondary size='massive' style={{ fontFamily: "'Open Sans', sans-serif" }}>
+              <Menu.Item header>Cryptoroses.us</Menu.Item>
+              <Menu.Menu position='right'>
+                <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+                <Menu.Item name='purchases' active={activeItem === 'purchases'} onClick={this.handleItemClick} />
+                <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} />
+              </Menu.Menu>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column></Grid.Column>
+        </Grid>
+
+        <div style={{ marginBottom: '60px' }}>
+          <Grid columns='equal'>
+            <Grid.Column></Grid.Column>
+            <Grid.Column width={8}>
+              {
+                (activeItem === 'home') ?
+                  <HomeSegment handleButtonClick={this.handleButtonClick} /> :
+                  (activeItem === 'buyViaETH') ?
+                    <BuyETH /> :
+                    (activeItem === 'buyViaGRLC') ?
+                      <p>Buy GRLC</p> :
+                      (activeItem === 'purchases') ?
+                        <p>scan hash id</p> :
+                        (activeItem === 'about') ?
+                          <p>about</p> :
+                          <p>How you end up here</p>
+              }
+            </Grid.Column>
+            <Grid.Column></Grid.Column>
+          </Grid>
+        </div>
+
+        <div style={{
+          position: 'fixed',
+          height: '60px',
+          bottom: '0px',
+          left: '0px',
+          right: '0px',
+          marginBottom: '0px'
+        }}>
+          <Grid columns='equal'>
+            <Grid.Column></Grid.Column>
+            <Grid.Column width={8}>
+              <hr />
+              <div style={{ textAlign: 'right' }}>
+                <h4 style={{ fontFamily: "'Open Sans', sans-serif" }}>&copy; CRYPTOROSES.US 2018</h4>
+              </div>
+            </Grid.Column>
+            <Grid.Column></Grid.Column>
+          </Grid>
+        </div>
+      </div >
+    )
+  }
+}
+
+export default CryptoRosesTemplate
